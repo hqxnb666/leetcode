ee592aba411b68363f71ea59e3ed1c3cafb06cae
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-06-28 00:10:27 +0800

    Add files via upload

---
+++ MFCApplication4Dlg(2).cpp
@@ -0,0 +1,206 @@
+﻿
+// MFCApplication4Dlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication4.h"
+#include "MFCApplication4Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication4Dlg 对话框
+
+
+
+CMFCApplication4Dlg::CMFCApplication4Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION4_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication4Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CMFCApplication4Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_CONNECT_BUTTON, &CMFCApplication4Dlg::OnBnClickedConnect)
+	ON_BN_CLICKED(IDC_SEND_BUTTON, &CMFCApplication4Dlg::OnBnClickedSend)
+	ON_BN_CLICKED(IDCANCEL, &CMFCApplication4Dlg::OnBnClickedCancel)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication4Dlg 消息处理程序
+
+BOOL CMFCApplication4Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CMFCApplication4Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMFCApplication4Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMFCApplication4Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CMFCApplication4Dlg::OnBnClickedConnect() {
+	CString strServerIP = _T("127.0.0.1"); // 服务器IP地址，假设为本地机器
+	int nPort = 6000; // 与服务器相同的端口号
+
+	MessageBox(_T("尝试连接服务器..."), _T("连接"), MB_ICONINFORMATION);
+
+	if (!m_clientSocket.Create()) {
+		MessageBox(_T("Socket 创建失败!"), _T("错误"), MB_ICONERROR);
+		return;
+	}
+
+	if (!m_clientSocket.Connect(strServerIP, nPort)) {
+		int nErrorCode = m_clientSocket.GetLastError();
+		CString strError;
+		strError.Format(_T("连接服务器失败，错误码: %d"), nErrorCode);
+		MessageBox(strError, _T("错误"), MB_ICONERROR);
+	}
+	else {
+		MessageBox(_T("成功连接到服务器!"), _T("连接"), MB_ICONINFORMATION);
+	}
+}
+
+
+
+
+void CMFCApplication4Dlg::OnBnClickedSend()
+{
+	CString message = _T("Hello, Server!");  // 需要发送的信息
+	int messageLength = message.GetLength() * sizeof(TCHAR);  // 计算消息长度（字节数）
+
+	// 显示消息框通知用户正在尝试发送信息
+	MessageBox(_T("尝试发送信息..."), _T("发送"), MB_ICONINFORMATION);
+
+	if (m_clientSocket.Send(LPCTSTR(message), messageLength) == SOCKET_ERROR) {
+		MessageBox(_T("发送信息失败!"), _T("错误"), MB_ICONERROR);
+	}
+	else {
+		MessageBox(_T("信息发送成功!"), _T("发送"), MB_ICONINFORMATION);
+	}
+}
+
+
+void CMFCApplication4Dlg::OnBnClickedCancel()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnCancel();
+}
